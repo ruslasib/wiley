b49242a17032543a78816abeb4fdb04c223e16c8
Ruslan Sibgatullin <rusla.sib@gmail.com>
2019-08-10 16:30:43 +0300

    Test for navigation panel is done.

--- src/test/java/ru/ruslasib/study/wiley/NavigationPanelTest.java
+++ src/test/java/ru/ruslasib/study/wiley/NavigationPanelTest.java
@@ -1,42 +1,23 @@
 package ru.ruslasib.study.wiley;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+public class NavigationPanelTest extends TestBase {
 
-public class NavigationPanelTest {
-
-  WebDriver wd;
-  @Before
-  public void setUp() {
-    String chromeDriverPath = System.getProperty("user.dir") + "\\src\\test\\java\\ru\\ruslasib\\study\\wiley\\resourses\\chromedriver.exe";
-    System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-    wd = new ChromeDriver();
-    wd.manage().window().maximize();
-    wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    wd.get("https://www.wiley.com/en-us");
-    wd.findElement(By.xpath("//*[@id=\"country-location-form\"]/div[3]/button[2]")).click();
+  @Test
+  public void testWhoWeServeOfNavigationPanel() {
+    Assert.assertTrue("Element " + "Who we serve" + " not existed", isElementPresent(By.linkText("WHO WE SERVE")));
   }
 
   @Test
-  public void testNavigationPanel() {
-    //System.out.println("Text: " + wd.findElement(By.linkText("WHO WE SERVE")).getText());
-    Assert.assertTrue("Element " + "WHO WE SERVE" + " not existed", isElementPresent(By.xpath("//*[@id=\"country-location-form\"]/div[3]/button[2]")));
+  public void testSubjectsOfNavigationPanel() {
+    Assert.assertTrue("Element " + "Subjects" + " not existed", isElementPresent(By.linkText("SUBJECTS")));
   }
 
-  public boolean isElementPresent(By locator) {
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    List<WebElement> webElementList = wd.findElements(locator);
-    if (webElementList.size() > 0) {
-      return true;
-    }
-    return false;
+  @Test
+  public void testAboutOfNavigationPanel() {
+    Assert.assertTrue("Element " + "About" + " not existed", isElementPresent(By.linkText("ABOUT")));
   }
 }

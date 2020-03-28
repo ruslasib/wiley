288447775110c7e3cbcb4d07287898a45601b3ad
Ruslan Sibgatullin <rusla.sib@gmail.com>
2020-03-29 00:28:39 +0300

    testWhoWeServeSubmenu doesn't work

--- src/test/java/ru/ruslasib/study/wiley/TopMenuTests.java
+++ src/test/java/ru/ruslasib/study/wiley/TopMenuTests.java
@@ -2,6 +2,10 @@ package ru.ruslasib.study.wiley;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -20,5 +24,12 @@ public class TopMenuTests extends TestBase {
   @Test
   public void testAboutMenuExist() {
     assertThat("ABOUT", equalTo(homePage.aboutName()));
+  }
+
+  @Test
+  public void testWhoWeServeSubmenu() {
+    assertThat(11, equalTo(homePage.whoWeServeSubmenuItems().size()));
+    List<String> items = Arrays.asList("Students", "Instructors", "Book Authors", "Professionals", "Researchers", "Institutions", "Librarians", "Corporations", "Societies", "Journal Editors", "Government");
+    assertThat(items, equalTo(homePage.whoWeServeSubmenuItems()));
   }
 }
